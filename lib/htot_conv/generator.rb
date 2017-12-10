60b568f844532353e1c2776c418361cc8ad1d29d
cat_in_136 <cat.in.136+github@gmail.com>
2017-12-10 22:36:55 +0900

    introduce type2 with rubyXL

--- lib/htot_conv/generator.rb
+++ lib/htot_conv/generator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'htot_conv/generator/xlsx_type0.rb'
 #require 'htot_conv/generator/xlsx_type1.rb'
-#require 'htot_conv/generator/xlsx_type2.rb'
+require 'htot_conv/generator/xlsx_type2.rb'
 #require 'htot_conv/generator/xlsx_type3.rb'
 require 'htot_conv/generator/xlsx_type4.rb'
 #require 'htot_conv/generator/xlsx_type5.rb'
